a5b01a7012c18c8371dcd5b70b2a4c8d5267dd09
Михаил Родичев <rodichevmichael@gmail.com>
2022-11-27 21:50:03 +0300

    первая отправка ревьюверу

--- homework.py
+++ homework.py
@@ -85,14 +85,6 @@ class SportsWalking(Training):
         super().__init__(action, duration, weight)
         self.height = height
 
-    #   def get_spent_calories(self) -> float:
-    #        """Считает калории для спортивной ходьбы"""
-    #        return ((self.CALORIES_WEIGHT_MULTIPLIER * self.weight
-    #                 + ((self.get_mean_speed() * self.KMH_IN_MSEC) ** 2
-    #                    / (self.height / self.CM_IN_M)
-    #                    * self.CALORIES_SPEED_HEIGHT_MULTIPLIER * self.weight)
-    #                 * (self.duration * self.MIN_IN_H)))
-
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
 
